cceb49e282553a5e3da7f1717cac4abdfd48a821
Christopher P. Cheng <forever.21@dartmouth.edu>
2018-10-23 11:40:48 -0400

    added cmdline functionality

--- scripts/convert_to_bids.py
+++ scripts/convert_to_bids.py
@@ -5,6 +5,21 @@
 
 import pydicom
 import re
+from optparse import OptionParser, Option
+
+
+def get_opt_parser():
+    # use module docstring for help output
+    p = OptionParser()
+
+    p.add_options([
+        Option("-p", "--placeholder",
+               dest="hopa", default="leaving room for future options to be added",
+               help="out of order"),
+
+    ])
+
+    return p
 
 
 def run_command(cmd):
@@ -34,5 +49,9 @@ def convert_tarball(filename):
     run_command('heudiconv --bids -f reproin -s {} -ss {} --files {}'.format(subjid, date, filename))
 
 if __name__ == '__main__':
+    parser = get_opt_parser()
+
+    (options, files) = parser.parse_args(args)
+
     for filename in files:
-       convert_tarball(filename)
+        convert_tarball(filename)
